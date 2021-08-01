634c68d4cb03ae8bc79bff8dcd74bc8b8c35e431
pra152 <pravatdas211@gmail.com>
2021-08-01 07:07:42 +0530

    Added : 4.Reverse Number

---
+++ src/com/bridgelabz/logicalprograms/ReverseNumber.java
@@ -0,0 +1,14 @@
+package com.bridgelabz.logicalprograms;
+public class ReverseNumber {
+	public static void main(String args[]) {
+		int num = 1234;
+		int rev = 0;
+		while (num != 0) {
+			int digit = num % 10;
+			rev = rev * 10 + digit;
+			num /= 10;
+		}
+		System.out.println("Reversed Number: " + rev);
+	}
+
+}
